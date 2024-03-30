718abe9e9da1755e688db64faae6de3119cf3cd7
Luz00 <igor.luizdsc@hotmail.com>
2024-03-30 20:41:46 -0300

    Update DAO.java
    teste

--- idadeAtiva/app/src/main/java/com/tcc/idadeativa/DAO/DAO.java
+++ idadeAtiva/app/src/main/java/com/tcc/idadeativa/DAO/DAO.java
@@ -12,7 +12,7 @@ import com.tcc.idadeativa.objetos.Pessoa;
 import java.util.AbstractList;
 import java.util.ArrayList;
 import java.util.List;
-
+// teste
 public class DAO extends SQLiteOpenHelper {
 
     public DAO (Context context){
